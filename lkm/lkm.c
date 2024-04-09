10c2dcb0a239c0ead6c6fc72e1709de7b0afc77d
Kuldip Patel <patel.kuldip91@gmail.com>
2024-04-10 05:16:44 +0530

    Fixed more and Added another sample driver

---
+++ lkm/lkm.c
@@ -0,0 +1,23 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init lkm1_init(void) {
+    printk(KERN_INFO "lkm1: %s", __FUNCTION__);
+    return 0;
+}
+
+static void __exit lkm1_exit(void) {
+    printk(KERN_INFO "lkm1: exit\n");
+}
+
+module_init(lkm1_init);
+module_exit(lkm1_exit);
+
+MODULE_AUTHOR("kp7742");
+MODULE_DESCRIPTION("lkm test");
+MODULE_LICENSE("MIT");
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
+MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif
